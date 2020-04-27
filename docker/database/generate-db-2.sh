2d9ff49f005baf6b5766f621a4ca7dd552347721
TsAah[THoF] <tsaah.thof@gmail.com>
2020-04-28 00:22:30 +0300

    more

--- docker/database/generate-db-2.sh
+++ docker/database/generate-db-2.sh
@@ -1,27 +1,8 @@
-#!/bin/sh 
-
-echo 'Importing databases'
-
-echo 'Importing logon'
+mysql_upgrade -u root -h 127.0.0.1 -p$MYSQL_ROOT_PASSWORD
 mysql -u root -h 127.0.0.1 -p$MYSQL_ROOT_PASSWORD realmd < /opt/vmangos/sql/logon.sql
-
-echo 'Importing logs'
 mysql -u root -h 127.0.0.1 -p$MYSQL_ROOT_PASSWORD logs < /opt/vmangos/sql/logs.sql
-
-echo 'Importing characters'
 mysql -u root -h 127.0.0.1 -p$MYSQL_ROOT_PASSWORD characters < /opt/vmangos/sql/characters.sql
-
-echo 'Importing world'
 mysql -u root -h 127.0.0.1 -p$MYSQL_ROOT_PASSWORD mangos < /opt/vmangos/sql/database/$world.sql
-echo 'Databases imported'
-
-echo 'Importing changes'
 mysql -u root -h 127.0.0.1 -p$MYSQL_ROOT_PASSWORD mangos < /opt/vmangos/sql/migrations/world_db_updates.sql
 mysql -u root -h 127.0.0.1 -p$MYSQL_ROOT_PASSWORD characters < /opt/vmangos/sql/migrations/characters_db_updates.sql
-
-echo 'Upgrading mysql'
-mysql_upgrade -u root -h 127.0.0.1 -p$MYSQL_ROOT_PASSWORD
-
-echo 'Configuring default realm'
 mysql -u root -h 127.0.0.1 -p$MYSQL_ROOT_PASSWORD -e "INSERT INTO realmd.realmlist (name, address, port, icon, realmflags, timezone, allowedSecurityLevel, population, gamebuild_min, gamebuild_max, flag, realmbuilds) VALUES ('$realm_name', '$realm_ip', '$realm_port', '$realm_icon', '$realmflags', '$timezone', '$allowedSecurityLevel', '$population', '$gamebuild_min', '$gamebuild_max', '$flag','');"
-echo 'Database creation is complete'
