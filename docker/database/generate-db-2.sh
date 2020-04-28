7deb9ee095162c0a1a06c9d2f80e00fc84cbe7e0
TsAah[THoF] <tsaah.thof@gmail.com>
2020-04-28 16:44:55 +0300

    dd

--- docker/database/generate-db-2.sh
+++ docker/database/generate-db-2.sh
@@ -6,3 +6,5 @@ mysql -u root -p$MYSQL_ROOT_PASSWORD mangos < /opt/vmangos/sql/migrations/world_
 mysql -u root -p$MYSQL_ROOT_PASSWORD characters < /opt/vmangos/sql/migrations/characters_db_updates.sql
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "INSERT INTO realmd.realmlist (name, address, port, icon, realmflags, timezone, allowedSecurityLevel, population, gamebuild_min, gamebuild_max, flag, realmbuilds) VALUES ('$realm_name', '$realm_ip', '$realm_port', '$realm_icon', '$realmflags', '$timezone', '$allowedSecurityLevel', '$population', '$gamebuild_min', '$gamebuild_max', '$flag','');"
 mysql_upgrade -u root -p$MYSQL_ROOT_PASSWORD
+
+rm /opt/vmangos/sql/database/$world.sql
